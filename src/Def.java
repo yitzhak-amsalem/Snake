eeccac608a93310de8ec88bbbab8cd6861055699
Yitzhak Amsalem <yitzhak@gmail.com>
2021-05-28 14:40:38 +0300

    Initial commit

---
+++ src/Def.java
@@ -0,0 +1,23 @@
+public class Def {
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 600;
+
+    public static final int CAR_START_X = 100;
+    public static final int CAR_START_Y = 100;
+    public static final int CAR_WIDTH = 200;
+    public static final int CAR_HEIGHT = 100;
+
+    public static final int DIRECTION_RIGHT = 1;
+    public static final int DIRECTION_LEFT = 2;
+    public static final int DIRECTION_UP = 3;
+    public static final int DIRECTION_DOWN = 4;
+
+    public static final int HEAD_X_LOCATION = 400;
+    public static final int HEAD_Y_LOCATION = 100;
+    public static final int HEAD_SIZE = 65;
+    public static final int SHOULDER_WIDTH = 100;
+
+    public static final int GAME_SPEED_FAST = 1;
+    public static final int GAME_SPEED_SLOW = 100;
+    public static final int GAME_SPEED = 25;
+}
